2d0945a5eab97e95fc301cd463f6c21d332ff093
Aleksandr Shelepin <57798122+graveart@users.noreply.github.com>
2023-04-05 16:37:01 +0300

    Update reindexer to v3.14.2

--- reindexer.rb
+++ reindexer.rb
@@ -2,9 +2,9 @@ class Reindexer < Formula
   env :std
   desc "is an fast document-oriented database im-memory database."
   homepage "https://github.com/restream/reindexer"
-  url "https://github.com/Restream/reindexer/archive/v3.14.1.zip"
-  version "3.14.1"
-  sha256 "63184b58ac433a59b1e83b548a2e8fc3839ac401d4975d8fb267996e4aa8d765"
+  url "https://github.com/Restream/reindexer/archive/v3.14.2.zip"
+  version "3.14.2"
+  sha256 "1478a2d297d92da1cdc51ef4ab08bb618f3b2c7eeafa5dfdf068312feb639783"
 
   head "https://github.com/restream/reindexer.git"
 
